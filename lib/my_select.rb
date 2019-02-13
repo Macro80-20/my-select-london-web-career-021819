9f1df41e4c36712ad46003bb03f768b9bf859d08
fernando <fernando_lopezc@live.com>
2019-02-13 12:31:10 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
+collection = [1,2,3,4,5]
 def my_select(collection)
- # your code here!
+  if block_given?
+    i =0
+    new_array = [] #does not modify the original appropriately
+    while i<collection.length
+      new_array << yield(collection[i])
+     collection.delete(nil)
+      i+=1
+    end
+  else
+    return
+  end
+  new_array
+end
+
+my_select(collection) do
+  |element|
+ element if element.even?
 end
